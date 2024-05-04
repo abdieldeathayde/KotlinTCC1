8f02d2dfa8ac05b562d7c7803bdff8998ebaed7e
Athay <abdieldeathayde67@gmail.com>
2024-05-03 21:53:35 -0300

    arrumando ImageView

--- app/src/main/java/com/example/tcc1_abdiel/SignInActivity.kt
+++ app/src/main/java/com/example/tcc1_abdiel/SignInActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
@@ -15,13 +16,13 @@ class SignInActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
 
-
-        val botao = findViewById<Button>(R.id.button)
         val email = findViewById<EditText>(R.id.emailEt)
         val pass = findViewById<EditText>(R.id.passET)
+        val logoIFSC = findViewById<ImageView>(R.id.imageView)
 
-        setContentView(R.layout.activity_sign_in)
+
 
         findViewById<TextView>(R.id.textView)?.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
@@ -31,27 +32,24 @@ class SignInActivity : Activity() {
 
         firebaseAuth = FirebaseAuth.getInstance()
 
-        if (botao != null) {
-            botao.setOnClickListener {
-                val email1 = email.text.toString()
-                val pass1 = pass.text.toString()
+        findViewById<Button>(R.id.button)?.setOnClickListener {
+            val email1 = email.text.toString()
+            val pass1 = pass.text.toString()
 
-                if (email1.isNotEmpty() && pass1.isNotEmpty()) {
+            if (email1.isNotEmpty() && pass1.isNotEmpty()) {
 
-                    firebaseAuth.signInWithEmailAndPassword(email1, pass1).addOnCompleteListener {
-                        if (it.isSuccessful) {
-                            val intent = Intent(this, MainActivity::class.java)
-                            startActivity(intent)
-                        } else {
-                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                firebaseAuth.signInWithEmailAndPassword(email1, pass1).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
 
-                        }
                     }
-                } else {
-                    Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT)
-                        .show()
-
                 }
+            } else {
+                Toast.makeText(this, "Campos não podem estar vazios !!", Toast.LENGTH_SHORT).show()
+
             }
         }
     }
--- app/src/main/java/com/example/tcc1_abdiel/SignUPActivity.kt
+++ app/src/main/java/com/example/tcc1_abdiel/SignUPActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
@@ -15,13 +16,14 @@ class SignUpActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val button1 = findViewById<Button>(com.example.tcc1_abdiel.R.id.button)
-        val emailEt = findViewById<EditText>(com.example.tcc1_abdiel.R.id.emailEt)
-        val passET = findViewById<TextView>(com.example.tcc1_abdiel.R.id.passET)
-        val confirmPassEt = findViewById<TextView>(com.example.tcc1_abdiel.R.id.confirmPassEt)
-
         setContentView(R.layout.activity_sign_up)
+
+        val emailEt = findViewById<EditText>(R.id.emailEt)
+        val passEt = findViewById<EditText>(R.id.passET)
+        val confirmPassEt = findViewById<EditText>(R.id.confirmPassEt)
+        val imageView = findViewById<ImageView>(R.id.imageView)
+
+
 
         firebaseAuth = FirebaseAuth.getInstance()
 
@@ -30,37 +32,34 @@ class SignUpActivity : Activity() {
             startActivity(intent)
         }
 
-        if(button1 != null) {
-            button1.setOnClickListener {
-                val email = emailEt.text.toString()
-                val pass = passET.text.toString()
-                val confirmPass = confirmPassEt.text.toString()
+        findViewById<Button>(R.id.button)?.setOnClickListener {
+            val email = emailEt.text.toString()
+            val pass = passEt.text.toString()
+            val confirmPass = confirmPassEt.text.toString()
 
-                if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
-                    if (pass == confirmPass) {
+            if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
+                if (pass == confirmPass) {
 
-                        firebaseAuth.createUserWithEmailAndPassword(email, pass)
-                            .addOnCompleteListener {
-                                if (it.isSuccessful) {
-                                    val intent = Intent(this, SignInActivity::class.java)
-                                    startActivity(intent)
-                                } else {
-                                    Toast.makeText(
-                                        this,
-                                        it.exception.toString(),
-                                        Toast.LENGTH_SHORT
-                                    ).show()
+                    firebaseAuth.createUserWithEmailAndPassword(email, pass)
+                        .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                val intent = Intent(this, SignInActivity::class.java)
+                                startActivity(intent)
+                            } else {
+                                Toast.makeText(
+                                    this,
+                                    it.exception.toString(),
+                                    Toast.LENGTH_SHORT
+                                ).show()
 
-                                }
                             }
-                    } else {
-                        Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
-                    }
+                        }
                 } else {
-                    Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT)
-                        .show()
-
+                    Toast.makeText(this, "Senhas não são iguais", Toast.LENGTH_SHORT).show()
                 }
+            } else {
+                Toast.makeText(this, "Campos não podem estar vazios !!", Toast.LENGTH_SHORT).show()
+
             }
         }
     }
